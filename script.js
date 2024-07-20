774c8ad9bfb98b2b6ace0baa8ac05bc05587d997
RisBrine <119113183+RisBrine404@users.noreply.github.com>
2024-07-20 21:11:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const correctPassword = 'aisarah';  // Password yang baru
+
+function checkPassword() {
+    const userInput = document.getElementById('password-input').value;
+    if (userInput === correctPassword) {
+        window.location.href = 'article.html';
+    } else {
+        alert('Incorrect password!');
+    }
+} 
